78b7ec8d662e7ad35f21d39a0d2b5a618797bccc
myhearthio <lalez21@gmail.com>
2026-04-28 16:04:45 -0500

    Add Erev/Ronan/Caleb subcategories to Connor's allowed list

--- tools.js
+++ tools.js
@@ -9,7 +9,7 @@ const FAMILY_CATS = [
   "housing", "utilities", "groceries", "dining", "transportation",
   "health", "shopping", "travel", "entertainment", "gifts_charity",
   "kids_activities", "childcare", "education",
-  "childcare_babysitters", "childcare_nanny", "childcare_other",
+  "childcare_babysitters", "childcare_nanny", "childcare_erev", "childcare_ronan", "childcare_caleb", "childcare_other",
   "taxes_professional", "needs_review",
   "other", "excluded",
 ];
